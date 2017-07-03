2eff10a0cecc41856a8d0d369b247bc0107adc28
Kenneth Tilton <kentilton@gmail.com>
2017-07-03 09:11:39 -0400

    backup

--- ConnectJS/public_html/js/tutorial/todo-mvc.js
+++ ConnectJS/public_html/js/tutorial/todo-mvc.js
@@ -1,8 +1,92 @@
+function todoMVC() {
+    return div({}, c => {
+        return [
+            section({ class: "todoapp"
+                    , name: "todoapp"
+                    , todos: cI(localStorage.getObject("todos-ConnectJS") || []
+                                , {observer: todosPersist})}
+                , c => { return [
+                        h1("todos")
+                        , header({class: "header"}
+                            , c => { return [
+                                    input({
+                                        class: "new-todo"
+                                        , placeholder: "What needs doing?"
+                                        , autofocus: true
+                                        , onchange: 'todoAdd'
+                                    })]
+                            })
+                        , section({class: "main", name: "mainsection"}, c => { return [
+                                input({id: "toggle-all", class: "toggle-all", type: "checkbox"})
+                                , label("Mark all as complete", {for: "toggle-all"})
+                                , ul({class: "todo-list"}, c => {
+                                    return todoLines(c.fm('todoapp').todos)
+                                })]
+                        })
+                        , todoFooter(c)]
+                })
+            , footer({class: "info"}
+                , c => [p({}, 'Created by... <a href="http://tiltontec.com">Kenneth Tilton')])]
+    })
+}
 
-function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
+function todoLines( todos) {
+    return todos.map( (todo)=>{
+        return li({val: todo
+                , class: cF(c=>{ return c.md.val.complete ? "complete" : null})}
+            , c => { return [
+                div({class: "view"}, c => {
+                    return [
+                        input({class: "toggle complete" /*cF(c=>{ return "toggle" +
+                                            (c.md.complete ? " complete":"")
+                                        }) */
+                            , type: "checkbox", checked: true
+                            , complete: cI( todo.complete)
+                            , onclick: 'todoToggleComplete'})
+                        , label(todo.value)
+                        , button(null, {class: "destroy"})]
+                })
+                , input({class: "edit", value: "Create a TodoMVC template"})]})})
+}
+
+function todoToggleComplete (dom, e) {
+    clg('togglecomplete!!!!!');
+    let md = jsDom[dom.id] // find the "shadow" JS object matching the event dom
+        md.complete = !md.complete;
+}
+
+// --- footer -------------------------------------------------------------
+
+function todoFooter (c) {
+    return footer({class: "footer"}, c => {
+        return [
+            span({ class: "todo-count"
+                , content: cF(c => {
+                    let remCt = c.fm('todoapp').todos
+                        .filter(todo => {
+                            return !todo.completed;
+                        }).length;
+                    return `<strong>${remCt}</strong> item${remCt === 1 ? '' : 's'} remaining`
+                })
+            })
+            , button("Clear completed", {
+                class: "clear-completed"
+                , hidden: cF(c => {
+                    return c.fm('todoapp')
+                            .todos
+                            .filter(todo => { return todo.completed})
+                            .length === 0
+                })
+            })]
+    })
+}
+// --- persistence -------------------------------------------------------
+
+function todosPersist (name, me, newv, priorv, c) {
+    if ( priorv !== kUnbound) {
+        localStorage.clear();
+        localStorage.setObject("todos-ConnectJS", newv);
+    }
 }
 
 function mkTodo(text) {
@@ -18,101 +102,6 @@ function todoAdd (dom, e) {
     appMd.todos = todos;
 }
 
-function todoToggleComplete (dom, e) {
-    let md = jsDom[dom.id] // find the "shadow" JS object matching the event dom
-        , appMd = md.fm('todoapp')
-        , todos = appMd.todos.slice();
-}
-function todoLines( todos) {
-    return todos.map( (todo)=>{
-        return li({val: todo
-                    , class: cF(c=>{ return c.md.val.complete ? "complete" : null})}
-            , c => { return [
-                div({class: "view"}, c => {
-                    return [
-                        input({class: "toggle", type: "checkbox", checked: true
-                            , complete: cI( todo.complete)
-                            , onclick: 'todoToggleComplete'})
-                        , label(todo.value)
-                        , button(null, {class: "destroy"})]
-                })
-                , input({class: "edit", value: "Create a TodoMVC template"})]})})
-}
-
-function todosPersist (name, me, newv, priorv, c) {
-    if ( priorv !== kUnbound) {
-        localStorage.clear();
-        localStorage.setObject("todos-ConnectJS", newv);
-    }
-}
-
-function todoMVC() {
-    return div({}, c => {
-        return [
-            section({
-                    class: "todoapp"
-                    , name: "todoapp"
-                    , todos: cI(localStorage.getObject("todos-ConnectJS") || []
-                                , {observer: todosPersist})
-                }
-                , c => {
-                    return [
-                        h1("todos")
-                        , header({class: "header"}
-                            , c => {
-                                return [
-                                    input({
-                                        class: "new-todo"
-                                        , placeholder: "What needs doing?"
-                                        , autofocus: true
-                                        , onchange: 'todoAdd'
-                                    })]
-                            })
-                        , section({class: "main", name: "mainsection"}, c => {
-                            return [
-                                input({id: "toggle-all", class: "toggle-all", type: "checkbox"})
-                                , label("Mark all as complete", {for: "toggle-all"})
-                                , ul({class: "todo-list"}, c => {
-                                    return todoLines(c.fm('todoapp').todos)
-                                })]
-                        })
-                        , todoFooter(c)]
-                })
-            , footer({class: "info"}, c => {
-                return [
-                    p({}, 'Created by <a href="http://tiltontec.com">Kenneth Tilton')]
-            })]
-    })
-}
-
-function todoFooter (c) {
-    return footer({class: "footer"}, c => {
-        return [
-            span({
-                class: "todo-count"
-                , content: cF(c => {
-                    let remCt = c.fm('todoapp')
-                        .todos
-                        .filter(todo => {
-                            return !todo.completed;
-                        })
-                        .length;
-                    return `<strong>${remCt}</strong> item${remCt === 1 ? '' : 's'} remaining`
-                })
-            })
-            , button("Clear completed", {
-                class: "clear-completed"
-                , hidden: cF(c => {
-                    return c.fm('todoapp')
-                            .todos
-                            .filter(todo => {
-                                return todo.completed
-                            })
-                            .length === 0
-                })
-            })]
-    })
-}
 
 /*
  <footer class="footer">
