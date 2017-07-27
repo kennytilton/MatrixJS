380694b7b5bc58fa9f7781977b7fa839dea45e16
Kenneth Tilton <kentilton@gmail.com>
2017-07-27 11:57:49 -0400

    get tabs->spaces in todo.js

--- js/matrixjs/js/Todo.js
+++ js/matrixjs/js/Todo.js
@@ -8,87 +8,87 @@
 const TODO_LS_PREFIX = "todos-MatrixJS.";
 
 class Todo extends Model {
-	constructor(islots) {
-		let netSlots = Object.assign(
-			{dbKey: TODO_LS_PREFIX + uuidv4(),
-			 created: Date.now()},
-			islots,
-			{ title: cI( islots.title ),
-				completed: todoCompletedRule( islots.completed),
-				deleted: islots.deleted || cI( null)});
+    constructor(islots) {
+        let netSlots = Object.assign(
+            {dbKey: TODO_LS_PREFIX + uuidv4(),
+             created: Date.now()},
+            islots,
+            { title: cI( islots.title ),
+                completed: todoCompletedRule( islots.completed),
+                deleted: islots.deleted || cI( null)});
 
-		super(null, null, netSlots, false); // flow:GLUE: Model constructor does a *lot*
+        super(null, null, netSlots, false); // flow:GLUE: Model constructor does a *lot*
 
-		if ( !islots.dbKey) { // ie, if not already stored, ie if being instantiated from DB
-			this.store();
-		}
-	}
-	toJSON () {
-		return  {dbKey: this.dbKey,
-				 title: this.title,
-				 created: this.created,
-				 completed: this.completed,
-				 deleted: this.deleted }
-	}
-	static fromJSON ( json) {
-		return new Todo( json )
-	}
-	static load (dbKey) {
-		return new Todo( localStorage.getObject( dbKey))
-	}
+        if ( !islots.dbKey) { // ie, if not already stored, ie if being instantiated from DB
+            this.store();
+        }
+    }
+    toJSON () {
+        return  {dbKey: this.dbKey,
+                 title: this.title,
+                 created: this.created,
+                 completed: this.completed,
+                 deleted: this.deleted }
+    }
+    static fromJSON ( json) {
+        return new Todo( json )
+    }
+    static load (dbKey) {
+        return new Todo( localStorage.getObject( dbKey))
+    }
 
-	static obsTodoChange ( slot, todo, newv, priorv, c) {
-		todo.store();
-	}
+    static obsTodoChange ( slot, todo, newv, priorv, c) {
+        todo.store();
+    }
 
-	slotObserverResolve(slot) {
-		// identifying to the Cells engine which slot observer to use
-		return Todo.obsTodoChange }
+    slotObserverResolve(slot) {
+        // identifying to the Cells engine which slot observer to use
+        return Todo.obsTodoChange }
 
-	static loadAllItems() {
-		return mkm( null, 'Todo',
-				{ itemsRaw: cI( Object.keys(localStorage)
-									.filter( k => k.startsWith(TODO_LS_PREFIX))
-									.map( Todo.load)
-									.sort( (a,b) => a.created < b.created ? -1 : 1) || []),
+    static loadAllItems() {
+        return mkm( null, 'Todo',
+                { itemsRaw: cI( Object.keys(localStorage)
+                                    .filter( k => k.startsWith(TODO_LS_PREFIX))
+                                    .map( Todo.load)
+                                    .sort( (a,b) => a.created < b.created ? -1 : 1) || []),
 
-				items: cF( c => c.md.itemsRaw.filter( td => !td.deleted)),
+                items: cF( c => c.md.itemsRaw.filter( td => !td.deleted)),
 
-				txBulkOp: cIe( null)})
-	}
-	store () {
-		localStorage.setObject( this.dbKey, this.toJSON());
-	}
-	delete() {
-		this.deleted = Date.now();
-	}
-	static completionRevise (targets, action) {
-		Todos.txBulkOp = {opCode: 'completion', targets: targets, action: action};
-	}
+                txBulkOp: cIe( null)})
+    }
+    store () {
+        localStorage.setObject( this.dbKey, this.toJSON());
+    }
+    delete() {
+        this.deleted = Date.now();
+    }
+    static completionRevise (targets, action) {
+        Todos.txBulkOp = {opCode: 'completion', targets: targets, action: action};
+    }
 }
 
 function todoCompletedRule( initialValue) {
-	// This is an example showing how complicated formulae can be broken out
-	// as functions just to keep source manageable.
+    // This is an example showing how complicated formulae can be broken out
+    // as functions just to keep source manageable.
 
-	return cF( c => {
-		let tx = Todos.txBulkOp;
+    return cF( c => {
+        let tx = Todos.txBulkOp;
 
-		if ( tx && tx.opCode === 'completion' && tx.targets.indexOf( c.md) !== -1) {
-			return tx.action === 'undo' ? null : (c.pv || Date.now()); // enforce not stomp on prior comp
-		} else {
-			// there is no tx (the model is coming to life) or this TD is not
-			// affected by the bulk TX, so stick with current/initial value:
-			if ( c.pv === kUnbound ) { // rule is running for first time
-				return initialValue; // reloaded from localStorage
-			} else {
-				return c.pv; // initialValue only gets used initially
-			}
-			// Advanced Cells: we could use so-called synaptic Cells inside rules
-			// when referencing the txBulkOp value to avoid this rule getting
-			// re-evaluated unnecessarily (when opCode not completion or this md
-			// not being on the target list).
-		}
-	})
+        if ( tx && tx.opCode === 'completion' && tx.targets.indexOf( c.md) !== -1) {
+            return tx.action === 'undo' ? null : (c.pv || Date.now()); // enforce not stomp on prior comp
+        } else {
+            // there is no tx (the model is coming to life) or this TD is not
+            // affected by the bulk TX, so stick with current/initial value:
+            if ( c.pv === kUnbound ) { // rule is running for first time
+                return initialValue; // reloaded from localStorage
+            } else {
+                return c.pv; // initialValue only gets used initially
+            }
+            // Advanced Cells: we could use so-called synaptic Cells inside rules
+            // when referencing the txBulkOp value to avoid this rule getting
+            // re-evaluated unnecessarily (when opCode not completion or this md
+            // not being on the target list).
+        }
+    })
 }
 
